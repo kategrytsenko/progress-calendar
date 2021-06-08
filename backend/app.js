069879fbd75b96ae26f0cd1b7fbe17dd39b7869f
kategrytsenko <grytsenko.kate.ua@gmail.com>
2021-06-08 14:04:07 +0300

    PK-5: connected database

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Task = require('./models/task');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://aktinijal:L68tD8UYqYQMg53@cluster0.vcg4r.mongodb.net/node-angular?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,8 +30,9 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/tasks', (req, res, next) => {
-  const task = req.body;
-  console.log(task);
+  const { name, startDate, endDate, iterance } = req.body;
+  const task = new Task({ name, startDate, endDate, iterance });
+  task.save();
   res.status(201).json({
     message: 'Task added successfully'
   });
